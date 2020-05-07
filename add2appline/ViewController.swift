3b500a55d30a20054f616269bfeb9567e1f3a697
Hiroshi Kikuchi <hiroshi.kikuchi.dvs@diverse-inc.com>
2020-05-07 16:26:50 +0900

    Initial Commit

---
+++ add2appline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  add2appline
+//
+//  Created by 菊池 紘 on 2020/05/07.
+//  Copyright © 2020 菊池 紘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
